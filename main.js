e15b875af64c0535ad5b912ae8e8f16791061c72
Lluis Casamajor <lluischess@gmail.com>
2021-05-24 23:44:11 +0200

    Tipo de datos javascript

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-
+// variables
 var nombre = "Hola";
 var anos = 19;
 
-alert("Hola")
-
+// mensajes y alertas
+alert("Hola");
 document.write(nombre);
+console.log(nombre);
 
+// Objetos
+var objeto = {
+    nombre: "Luis",
+    apellido: "casamajor"
+};
+console.log(objeto);
+// Tipo de dato symbol
+var simbol = Symbol("mi simbolo");
+
+// saber tipo de variable
+console.log(typeof objeto);
